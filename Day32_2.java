63804e6967dbbaa7d96a8ffd1eb02c2c6143c4da
Saptarshi Sen <160701777+saptarshi-sen1@users.noreply.github.com>
2025-06-16 23:56:06 +0530

    Create Day32_2.java

---
+++ Day32_2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Day32_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        int[] nums = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            nums[i] = Integer.parseInt(input[i]);
+        }
+        int target = sc.nextInt();
+        int index = searchInsert(nums, target);
+        System.out.println(index);
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+}
