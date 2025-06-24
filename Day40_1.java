3e70a2e6ce2eff62c4430f5a1ef9e3395c8ecc28
Saptarshi Sen <160701777+saptarshi-sen1@users.noreply.github.com>
2025-06-24 23:21:16 +0530

    Update Day40_1.java

--- Day40_1.java
+++ Day40_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RecursivePower {
+public class Day40_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double x = sc.nextDouble();
