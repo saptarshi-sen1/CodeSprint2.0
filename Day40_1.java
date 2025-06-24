762a7f9ba23f3584d4cff30bed1688c43d0e5564
Saptarshi Sen <160701777+saptarshi-sen1@users.noreply.github.com>
2025-06-24 23:21:00 +0530

    Create Day40_1.java

---
+++ Day40_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RecursivePower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        int n = sc.nextInt();
+        double result = power(x, n);
+        System.out.printf("%.5f\n", result);
+    }
+
+    public static double power(double x, int n) {
+        if (n == 0)
+            return 1;
+        double half = power(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
+        else if (n > 0)
+            return x * half * half;
+        else
+            return (half * half) / x;
+    }
+}
