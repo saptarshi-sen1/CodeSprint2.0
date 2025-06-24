66724dca197583fb6f18ed32c16514b3ea3d5d0a
Saptarshi Sen <160701777+saptarshi-sen1@users.noreply.github.com>
2025-06-24 23:22:01 +0530

    Create Day40_2.java

---
+++ Day40_2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day40_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            for (int j = 0; j < i; j++)
+                dp[i] += dp[j] * dp[i - j - 1];
+        System.out.println(dp[n]);
+    }
+}
+// the above program is using dynamic programming 
